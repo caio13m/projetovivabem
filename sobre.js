c11bbad7e4fc28d4eccbac70525aaec61aeac196
caio13m <caioconfirma@gmail.com>
2024-08-17 16:28:20 -0300

    solindoss

--- sobre.js
+++ sobre.js
@@ -1,8 +1,19 @@
 const desc = document.querySelector(".desc p")
+const descORGtexto = desc.textContent
+let limite = document.body.getBoundingClientRect();
 
-const limite = document.body.getBoundingClientRect();
-console.log(limite.width*0.1)
+function update(){
+    limite = document.body.getBoundingClientRect();
+    console.log(limite.width*0.14)
 
-if (desc.textContent.length > limite) {
-    desc.textContent = desc.textContent.substring(0, limite.width*0.1);
+    if (descORGtexto.length > limite.width*0.14) {
+        desc.textContent = descORGtexto.substring(0, (limite.width*0.14))+'...';
+    }
 }
+// Não ta nem perto da meió forma de faze iso, MELHORA
+
+console.log(limite.width*0.15)
+console.log(desc.textContent.length)
+
+
+setInterval(update, 1000);
